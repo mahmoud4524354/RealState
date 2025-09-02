ebfc17da0d861c7d5b928640461d920d003e8d08
Mahmoud_Magdy <mahmoud30204050@gmail.com>
2025-09-02 11:44:21 +0300

    Frontend Template Setup

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
         @csrf
 
         <!-- Name -->
-        <div>
+        <div> 
             <x-input-label for="name" :value="__('Name')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
@@ -44,7 +44,7 @@
                 {{ __('Already registered?') }}
             </a>
 
-            <x-primary-button class="ms-4">
+            <x-primary-button class="ml-4">
                 {{ __('Register') }}
             </x-primary-button>
         </div>
