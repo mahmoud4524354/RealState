96476f8341ca1221e8de0883d8d7eeb63b570da9
Mahmoud_Magdy <mahmoud30204050@gmail.com>
2025-09-11 13:59:16 +0300

    Add property from agent dashboard

---
+++ app/Http/Controllers/Agent/AgentPropertyController.php
@@ -0,0 +1,360 @@
+<?php
+
+namespace App\Http\Controllers\Agent;
+
+use App\Http\Controllers\Controller;
+use App\Traits\FileUploadTrait;
+use Illuminate\Http\Request;
+use App\Models\Property;
+use App\Models\MultiImage;
+use App\Models\Facility;
+use App\Models\Amenities;
+use App\Models\PropertyType;
+use Intervention\Image\Facades\Image;
+use Haruncpi\LaravelIdGenerator\IdGenerator;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
+
+class AgentPropertyController extends Controller
+{
+    use FileUploadTrait;
+
+    public function AgentAllProperty()
+    {
+
+        $id = Auth::user()->id;
+        $property = Property::where('agent_id', $id)->latest()->get();
+        return view('agent.property.all_property', compact('property'));
+
+    } // End Method
+
+
+    public function AgentAddProperty()
+    {
+
+        $propertytype = PropertyType::latest()->get();
+        $amenities = Amenities::latest()->get();
+
+        return view('agent.property.add_property', compact('propertytype', 'amenities'));
+
+    }// End Method
+
+    public function AgentStoreProperty(Request $request)
+    {
+
+        $amen = $request->amenities_id;
+        $amenites = implode(",", $amen);
+        // dd($amenites);
+
+        $pcode = IdGenerator::generate(['table' => 'properties', 'field' => 'property_code', 'length' => 5, 'prefix' => 'PC']);
+
+
+        $image = $this->uploadImage($request, 'property_thambnail') ?? 'uploads/no_image.jpg';
+
+        $property_id = Property::insertGetId([
+
+            'ptype_id' => $request->ptype_id,
+            'amenities_id' => $amenites,
+            'property_name' => $request->property_name,
+            'property_slug' => strtolower(str_replace(' ', '-', $request->property_name)),
+            'property_code' => $pcode,
+            'property_status' => $request->property_status,
+
+            'lowest_price' => $request->lowest_price,
+            'max_price' => $request->max_price,
+            'short_descp' => $request->short_descp,
+            'long_descp' => $request->long_descp,
+            'bedrooms' => $request->bedrooms,
+            'bathrooms' => $request->bathrooms,
+            'garage' => $request->garage,
+            'garage_size' => $request->garage_size,
+
+            'property_size' => $request->property_size,
+            'property_video' => $request->property_video,
+            'address' => $request->address,
+            'city' => $request->city,
+            'state' => $request->state,
+            'postal_code' => $request->postal_code,
+
+            'neighborhood' => $request->neighborhood,
+            'latitude' => $request->latitude,
+            'longitude' => $request->longitude,
+            'featured' => $request->featured,
+            'hot' => $request->hot,
+            'agent_id' => Auth::user()->id,
+            'status' => 1,
+            'property_thambnail' => $image,
+            'created_at' => Carbon::now(),
+        ]);
+
+        /// Multiple Image Upload From Here ////
+        $images = $request->file('multi_img');
+        if ($images) {
+            foreach ($images as $img) {
+                $ext = $img->getClientOriginalExtension();
+                $imageName = 'media_' . uniqid() . '.' . $ext;
+                $path = "/uploads";
+                $img->move(public_path($path), $imageName);
+
+                MultiImage::insert([
+                    'property_id' => $property_id,
+                    'photo_name' => $path . '/' . $imageName,
+                    'created_at' => Carbon::now(),
+                ]);
+            }
+        }
+
+        /// End Multiple Image Upload From Here ////
+
+        /// Facilities Add From Here ////
+
+        $facilities = Count($request->facility_name);
+
+        if ($facilities != NULL) {
+            for ($i = 0; $i < $facilities; $i++) {
+                $fcount = new Facility();
+                $fcount->property_id = $property_id;
+                $fcount->facility_name = $request->facility_name[$i];
+                $fcount->distance = $request->distance[$i];
+                $fcount->save();
+            }
+        }
+
+        /// End Facilities  ////
+        toastr()->success('Property added successfully!');
+        return redirect()->route('agent.all.property');
+
+    }// End Method
+
+
+    public function AgentEditProperty($id)
+    {
+
+        $facilities = Facility::where('property_id', $id)->get();
+        $property = Property::findOrFail($id);
+
+        $type = $property->amenities_id;
+        $property_ami = explode(',', $type);
+
+        $multiImage = MultiImage::where('property_id', $id)->get();
+
+        $propertytype = PropertyType::latest()->get();
+        $amenities = Amenities::latest()->get();
+
+        return view('agent.property.edit_property', compact('property', 'propertytype', 'amenities', 'property_ami', 'multiImage', 'facilities'));
+
+    }// End Method
+
+    public function AgentUpdateProperty(Request $request)
+    {
+
+        $amen = $request->amenities_id;
+        $amenites = implode(",", $amen);
+
+        $property_id = $request->id;
+
+        Property::findOrFail($property_id)->update([
+
+            'ptype_id' => $request->ptype_id,
+            'amenities_id' => $amenites,
+            'property_name' => $request->property_name,
+            'property_slug' => strtolower(str_replace(' ', '-', $request->property_name)),
+            'property_status' => $request->property_status,
+
+            'lowest_price' => $request->lowest_price,
+            'max_price' => $request->max_price,
+            'short_descp' => $request->short_descp,
+            'long_descp' => $request->long_descp,
+            'bedrooms' => $request->bedrooms,
+            'bathrooms' => $request->bathrooms,
+            'garage' => $request->garage,
+            'garage_size' => $request->garage_size,
+
+            'property_size' => $request->property_size,
+            'property_video' => $request->property_video,
+            'address' => $request->address,
+            'city' => $request->city,
+            'state' => $request->state,
+            'postal_code' => $request->postal_code,
+
+            'neighborhood' => $request->neighborhood,
+            'latitude' => $request->latitude,
+            'longitude' => $request->longitude,
+            'featured' => $request->featured,
+            'hot' => $request->hot,
+            'agent_id' => Auth::user()->id,
+            'updated_at' => Carbon::now(),
+
+        ]);
+
+        toastr()->success('Property Updated Successfully');
+
+        return redirect()->route('agent.all.property');
+
+    }// End Method
+
+
+    public function AgentUpdatePropertyThambnail(Request $request)
+    {
+
+        $pro_id = $request->id;
+        $oldImage = $request->old_img;
+
+
+        if ($request->hasFile('property_thambnail')) {
+            $save_url = $this->uploadImage($request, 'property_thambnail');
+        } else {
+            $save_url = $oldImage;
+        }
+
+
+        Property::findOrFail($pro_id)->update([
+
+            'property_thambnail' => $save_url,
+            'updated_at' => Carbon::now(),
+        ]);
+
+        toastr()->success('Property Updated Successfully');
+
+        return redirect()->back();
+
+    }// End Method
+
+
+    public function AgentUpdatePropertyMultiimage(Request $request)
+    {
+        $imgs = $request->multi_img;
+
+        foreach ((array)$imgs as $id => $img) {
+            $imgDel = MultiImage::findOrFail($id);
+
+            $uploadPath = $this->uploadImage(
+                $request,
+                "multi_img.$id",
+                $imgDel->photo_name,
+                'uploads'
+            );
+
+            if ($uploadPath) {
+                $imgDel->update([
+                    'photo_name' => $uploadPath,
+                    'updated_at' => now(),
+                ]);
+            }
+        }
+
+        toastr()->success('Property Multi Image Updated Successfully');
+        return redirect()->back();
+    }
+
+//============================================
+
+    public function AgentPropertyMultiimgDelete($id)
+    {
+        $image = MultiImage::findOrFail($id);
+
+        $this->removeImage($image->photo_name);
+
+        $image->delete();
+
+        toastr()->success('Property Multi Image Deleted Successfully');
+
+        return back();
+    }
+
+
+    public function AgentStoreNewMultiimage(Request $request)
+    {
+
+        $new_multi = $request->imageid;
+
+        $uploadPath = $this->uploadImage($request, 'multi_img', 'uploads');
+
+        MultiImage::insert([
+            'property_id' => $new_multi,
+            'photo_name' => $uploadPath,
+            'created_at' => \Illuminate\Support\Carbon::now(),
+        ]);
+
+        toastr()->success('Multi Image Added Successfully');
+
+        return redirect()->back();
+    }// End Method
+
+
+    public function AgentUpdatePropertyFacilities(Request $request)
+    {
+
+        $pid = $request->id;
+
+        if ($request->facility_name == NULL) {
+            return redirect()->back();
+        } else {
+            Facility::where('property_id', $pid)->delete();
+
+            $facilities = Count($request->facility_name);
+
+            for ($i = 0; $i < $facilities; $i++) {
+                $fcount = new Facility();
+                $fcount->property_id = $pid;
+                $fcount->facility_name = $request->facility_name[$i];
+                $fcount->distance = $request->distance[$i];
+                $fcount->save();
+            }
+        }
+
+        toastr()->success('Property Facilities Updated Successfully');
+
+        return redirect()->back();
+
+    }// End Method
+
+
+    public function AgentDetailsProperty($id)
+    {
+
+        $facilities = Facility::where('property_id', $id)->get();
+        $property = Property::findOrFail($id);
+
+        $type = $property->amenities_id;
+        $property_ami = explode(',', $type);
+
+        $multiImage = MultiImage::where('property_id', $id)->get();
+
+        $propertytype = PropertyType::latest()->get();
+        $amenities = Amenities::latest()->get();
+
+
+        return view('agent.property.details_property', compact('property', 'propertytype', 'amenities', 'property_ami', 'multiImage', 'facilities'));
+
+    }// End Method
+
+
+    public function AgentDeleteProperty($id)
+    {
+
+        $property = Property::findOrFail($id);
+        $this->removeImage($property->property_thambnail);
+
+        Property::findOrFail($id)->delete();
+
+        $image = MultiImage::where('property_id', $id)->get();
+
+        foreach ($image as $img) {
+            $this->removeImage($img->photo_name);
+        }
+
+        $facilitiesData = Facility::where('property_id', $id)->get();
+        foreach ($facilitiesData as $item) {
+            $item->facility_name;
+            Facility::where('property_id', $id)->delete();
+        }
+
+        toastr()->success('Property Deleted Successfully');
+
+        return redirect()->back();
+
+    }// End Method
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Agent\AgentPropertyController;
 use App\Http\Controllers\AgentController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Backend\PropertyController;
@@ -132,6 +133,37 @@ Route::middleware(['auth', 'role:agent'])->group(function () {
     Route::get('/agent/change/password', [AgentController::class, 'agentChangePassword'])->name('agent.change.password');
 
     Route::post('/agent/update/password', [AgentController::class, 'agentUpdatePassword'])->name('agent.update.password');
+
+
+    // Agent All Property
+    Route::controller(AgentPropertyController::class)->group(function(){
+
+        Route::get('/agent/all/property', 'AgentAllProperty')->name('agent.all.property');
+        Route::get('/agent/add/property', 'AgentAddProperty')->name('agent.add.property');
+
+        Route::post('/agent/store/property', 'AgentStoreProperty')->name('agent.store.property');
+
+        Route::get('/agent/edit/property/{id}', 'AgentEditProperty')->name('agent.edit.property');
+
+        Route::post('/agent/update/property', 'AgentUpdateProperty')->name('agent.update.property');
+
+        Route::post('/agent/update/property/thambnail', 'AgentUpdatePropertyThambnail')->name('agent.update.property.thambnail');
+
+        Route::post('/agent/update/property/multiimage', 'AgentUpdatePropertyMultiimage')->name('agent.update.property.multiimage');
+
+        Route::get('/agent/property/multiimg/delete/{id}', 'AgentPropertyMultiimgDelete')->name('agent.property.multiimg.delete');
+
+        Route::post('/agent/store/new/multiimage', 'AgentStoreNewMultiimage')->name('agent.store.new.multiimage');
+
+        Route::post('/agent/update/property/facilities', 'AgentUpdatePropertyFacilities')->name('agent.update.property.facilities');
+
+        Route::get('/agent/details/property/{id}', 'AgentDetailsProperty')->name('agent.details.property');
+
+        Route::get('/agent/delete/property/{id}', 'AgentDeleteProperty')->name('agent.delete.property');
+
+
+
+    });
 
 });
 
