505441e7c056366d4c904498bdb2e22702933975
Mahmoud_Magdy <mahmoud30204050@gmail.com>
2025-10-19 22:14:21 +0300

    remove unnecessary code from agent dashboard

--- resources/views/agent/body/sidebar.blade.php
+++ resources/views/agent/body/sidebar.blade.php
@@ -90,13 +90,6 @@
 
             @endif
 
-            <li class="nav-item nav-category">Docs</li>
-            <li class="nav-item">
-                <a href="#" target="_blank" class="nav-link">
-                    <i class="link-icon" data-feather="hash"></i>
-                    <span class="link-title">Documentation</span>
-                </a>
-            </li>
         </ul>
     </div>
 </nav>
